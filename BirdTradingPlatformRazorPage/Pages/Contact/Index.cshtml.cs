b5bf1174492681c446675d23c5fee74f9bfcf9ce
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-06-07 19:47:01 +0700

    setup UI for website

---
+++ BirdTradingPlatformRazorPage/Pages/Contact/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BirdTradingPlatformRazorPage.Pages.Contact
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BirdTradingPlatformRazorPage/Pages/Shop/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BirdTradingPlatformRazorPage.Pages.Shop
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
