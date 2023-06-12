31de609dc00705a2865e0b77f0caf5c1f430c517
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-06-12 14:27:23 +0700

    update entity

--- DAO/OrderDAO.cs
+++ DAO/OrderDAO.cs
@@ -1,3 +1,5 @@
+using BussinessObject.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,52 @@ namespace DAO
 {
     public class OrderDAO
     {
+        public static List<Order> GetOrdersByUserId(int userId)
+        {
+            List<Order> orders = new List<Order>();
+            try
+            {
+                using var context = new BirdTradingPlatformContext();
+                orders = context.Orders
+                    .Include(p => p.PaymentMethod)
+                    .Include(p => p.OrderParent)
+                    .Where(o => o.PaymentMethod.UserId == userId)
+                    .Where(o => o.OrderParent != null)
+                    .ToList();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return orders;
+        }
+
+        public static void AddOrder(Order order)
+        {
+            try
+            {
+                using var context = new BirdTradingPlatformContext();
+                context.Orders.Add(order);
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static void DeleteOrder(Order order)
+        {
+            try
+            {
+                using var context = new BirdTradingPlatformContext();
+                context.Orders.Remove(order);
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- Repository/Implement/OrderRepository.cs
+++ Repository/Implement/OrderRepository.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using BussinessObject.Models;
+using DAO;
+using DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +10,19 @@ using System.Threading.Tasks;
 
 namespace Repository.Interface
 {
-    public class OrderRepository : IOrderRepository 
+    public class OrderRepository : IOrderRepository
     {
+        private readonly IMapper _mapper;
+
+        public OrderRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public List<OrderDTO> GetOrdersByUserId(int userId)
+        {
+            List<Order> list = OrderDAO.GetOrdersByUserId(userId);
+            return _mapper.Map<List<OrderDTO>>(list);
+        }
     }
 }
