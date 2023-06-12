dc8fbe3db0dcde7b11f3f75368e9f64397d62290
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-06-12 23:02:47 +0700

    update layout

--- BirdTradingPlatformRazorPage/Pages/Checkout/Index.cshtml.cs
+++ BirdTradingPlatformRazorPage/Pages/Checkout/Index.cshtml.cs
@@ -1,12 +1,48 @@
+using DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Interface;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace BirdTradingPlatformRazorPage.Pages.Checkout
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IPaymentMethodRepository _paymentMethodRepository;
+
+        public IndexModel(
+            IOrderDetailRepository orderDetailRepository, 
+            IOrderRepository orderRepository, 
+            IUserRepository userRepository, 
+            IPaymentMethodRepository paymentMethodRepository)
         {
+            _orderDetailRepository = orderDetailRepository;
+            _orderRepository = orderRepository;
+            _userRepository = userRepository;
+            _paymentMethodRepository = paymentMethodRepository;
+        }
+
+        public List<CartItemDTO> cartItems { get; set; }
+
+        public IActionResult OnGet()
+        {
+            string cart = HttpContext.Session.GetString("cart");
+
+            if (cart == null)
+            {
+                cartItems = new List<CartItemDTO>();
+            }
+            else if (cart != null)
+            {
+                cartItems = JsonSerializer.Deserialize<List<CartItemDTO>>(cart);
+            }
+
+            return Page();
         }
     }
 }
